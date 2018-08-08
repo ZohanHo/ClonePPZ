029dc9e4a463d11a70ddd90bd54c2f88c1a05000
ZohanHo <serduk.evgeniy@gmail.com>
2018-08-08 20:22:01 +0300

    08.08.2018

---
+++ all python/lesson 22.py
@@ -0,0 +1,9 @@
+"""Кортежи - tuple"""
+
+s = ("one", "Two", "three", "four", "five")
+
+w = "one", "Two", "three", "four", "five"  # Можно без скобок
+print(w[0])
+
+
+
---
+++ all python/lesson 25.py
@@ -0,0 +1,22 @@
+"""Функции обработки строк"""
+
+print(", ".join(["spam", "eggs", "ham"]))
+#spam, eggs, ham
+
+print("Hello ME".replace("ME", "world")) # Замещает одну строку на другую
+#prints Hello world
+
+print("This is a sentence.".startswith("This"))  # Определят есть ли строка в начале
+# prints "True"
+
+print("This is a sentence.".endswith("sentence.")) # Определят есть ли строка в конце
+# prints "True"
+
+print("This is a sentence.".upper()) # Вся строка большими буквами
+# prints "THIS IS A SENTENCE."
+
+print("AN ALL CAPS SENTENCE".lower())  # Вся строка маленькими буквами
+#prints "an all caps sentence"
+
+print("spam, eggs, ham".split(", ")) # Каждое слово становится строкой в списке
+#prints "['spam', 'eggs', 'ham']"
